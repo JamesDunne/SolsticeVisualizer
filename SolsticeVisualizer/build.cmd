f9dde5e3a619f745a5550422d6f61cac0098e1d1
James Dunne <james.jdunne@gmail.com>
2013-10-27 19:48:59 -0500

    Updated build scripts.

--- SolsticeVisualizer/build.cmd
+++ SolsticeVisualizer/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 SET OPENTK=D:\Software\OpenTK\1.0\Binaries\OpenTK\Release\OpenTK.dll
 mkdir bin\Release\
-csc /out:bin\Release\MainWindow.exe *.cs Properties\*.cs /r:%OPENTK% /r:System.dll /r:System.Drawing.dll /r:System.Windows.Forms.dll
+%WINDIR%\Microsoft.NET\Framework\v3.5\csc.exe /out:bin\Release\MainWindow.exe *.cs Properties\*.cs /r:%OPENTK% /r:System.dll /r:System.Drawing.dll /r:System.Windows.Forms.dll
 copy %OPENTK% bin\Release\
 copy "Solstice (U).nes" bin\Release\
 copy "Solstice (U).pal" bin\Release\
